85131826c7fa7fe3f27b242363817c83eb61d97d
Allie Soldau <alliesoldau@gmail.com>
2022-10-31 21:24:07 -0400

    commit tree ff54a2b46dc751a6c8791a80bcbb8d9bdc74efd5

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'seth';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'aaron';
+    return leastFavoriteCustomer;
+}
